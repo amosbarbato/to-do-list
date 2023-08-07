0b3be777beeaf36679426b3a68bad4f79f228592
Amos <amosbarbato@gmail.com>
2023-08-07 15:14:24 -0300

    Filtro

--- src/containers/ListaDeTarefas.tsx
+++ src/containers/ListaDeTarefas.tsx
@@ -9,6 +9,13 @@ export const Container = styled.main`
   overflow-y: scroll;
 `
 
+const Resultado = styled.p`
+  font-size: 18px;
+  font-weight: 700;
+  padding-top: 40px;
+  padding-bottom: 40px;
+`
+
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
   const { termo, criterio, valor } = useSelector(
@@ -38,16 +45,26 @@ const ListaDeTarefas = () => {
     }
   }
 
+  const exibeMensagem = (quantidade: number) => {
+    let mensagem = ''
+    const complementacao =
+      termo !== undefined && termo.length > 0 ? `e "${termo}"` : ''
+
+    if (criterio === 'todas') {
+      mensagem = `${quantidade} tarefa(s) encontrada(s) como: todas ${complementacao}`
+    } else {
+      mensagem = `${quantidade} tarefa(s) encontrada(s) como: "${`${criterio}=${valor}`}" ${complementacao}`
+    }
+
+    return mensagem
+  }
+
+  const tarefas = filtraTarefas()
+  const mensagem = exibeMensagem(tarefas.length)
+
   return (
     <Container>
-      <p>
-        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;
-      </p>
-      <ul>
-        <li>{termo}</li>
-        <li>{criterio}</li>
-        <li>{valor}</li>
-      </ul>
+      <Resultado>{mensagem}</Resultado>
       <ul>
         {filtraTarefas().map((t) => (
           <li key={t.titulo}>
